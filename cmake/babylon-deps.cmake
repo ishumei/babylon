0df096dd0051ec968270b8f75e43b96f50635ee4
day253 <9634619+day253@users.noreply.github.com>
2024-07-07 21:03:54 +0800

    remove useless

--- cmake/babylon-deps.cmake
+++ cmake/babylon-deps.cmake
@@ -1,9 +1,3 @@
 if(NOT TARGET absl::base)
   find_package(absl REQUIRED)
 endif()
-
-if(TARGET Boost::boost)
-elseif(TARGET Boost::preprocessor AND TARGET Boost::spirit)
-else()
-  find_package(Boost REQUIRED)
-endif()
